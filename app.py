79ba54d105fe28f8f47ae482cf37b649911d19b6
Izabela Lungu <izabelalungu9@gmail.com>
2025-07-01 12:46:52 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, send_file
+from PIL import Image, ImageDraw
+import io
+
+app = Flask(__name__)
+
+@app.route('/analyze', methods=['POST'])
+def analyze_image():
+    img = Image.open(request.files['file'])
+    draw = ImageDraw.Draw(img)
+    # Exemplu simplu: linie pe diagonală
+    draw.line((0, 0) + img.size, fill="red", width=5)
+    # Poți adăuga aici griduri/unghiuri...
+
+    img_io = io.BytesIO()
+    img.save(img_io, 'JPEG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
+if __name__ == "__main__":
+    app.run()
